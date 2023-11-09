7afdbbbb275f7ca12e6315be2c485324e8b11765
Quazi Samiha <quazisamiha@gmail.com>
2023-11-08 21:45:25 -0800

    tailwind css added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Learn1 from "./components/Learn1/Learn1";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-3xl my-6">
+          React TypeScript Learning Project
+        </h1>
+        <Learn1 />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Learn1/Learn1.tsx
@@ -0,0 +1,16 @@
+import User from "./User";
+
+const Learn1 = () => {
+  return (
+    <>
+      <section>
+        <h1>Learning Part 1: Built in Props Types</h1>
+        <div>
+          <User name="samiha tasnim" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Learn1;
